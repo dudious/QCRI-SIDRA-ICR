d41ca8733fc22057e6cce48865b3361ae654b277
Wouter <wouter@cse-node-01.sfdrwbdyldxura5qb1jwayl5dc.bx.internal.cloudapp.net>
2016-08-21 09:46:39 +0000

    biolinks on server

--- Any Cancer/1.5.Download.BIOLINKS.DATA.ANY-CANCER.V3.R
+++ Any Cancer/1.5.Download.BIOLINKS.DATA.ANY-CANCER.V3.R
@@ -18,13 +18,13 @@
 
 # Setup environment
 rm(list=ls())
-setwd("~/Dropbox (TBI-Lab)/BREAST_QATAR")
+#setwd("~/Dropbox (TBI-Lab)/BREAST_QATAR")
+setwd("/mnt3/wouter/BREAST-QATAR/")
 ## dependencies
-## download TCGA assembler scripts http://www.compgenome.org/TCGA-Assembler/
 #required.packages <- c("HGNChelper","RCurl","httr","stringr","digest","bitops")
 #missing.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
 #if(length(missing.packages)) install.packages(missing.packages)
-required.packages.BioC <- c("TCGAbiolinks")
+required.packages.BioC <- c("TCGAbiolinks","SummarizedExperiment")
 missing.packages <- required.packages.BioC[!(required.packages.BioC %in% installed.packages()[,"Package"])]
 source("http://bioconductor.org/biocLite.R")
 if(length(missing.packages)) biocLite(missing.packages)
@@ -34,7 +34,7 @@ library("SummarizedExperiment")
 
 # Set Parameters
 download.source     <- "TCGA"
-Cancerset           <- "OV"
+Cancerset           <- "BLCA"
 
 # Paths and flies
 Download.path       <- paste0("./2 DATA/",download.source," RNAseq/RNASeq_",Cancerset,"_BIOLINKS/")
@@ -43,7 +43,7 @@ Download.file       <- paste0(Cancerset,".RNASeq.",download.source,".BIOLINKS.DA
 # Download RNASeq data
 start.time <- Sys.time ()
 
-#LEGACY
+## LEGACY
 #query <- GDCquery(project = paste0(download.source,"-",Cancerset),
 #                  data.category = "Gene expression",
 #                  data.type = "Gene expression quantification",
@@ -51,7 +51,7 @@ start.time <- Sys.time ()
 #                  platform = "Illumina HiSeq",
 #                  sample.type = "Primary solid Tumor")
 
-#HARMONIZED
+## HARMONIZED
 query <- GDCquery(project = paste0(download.source,"-",Cancerset),
                   data.category = "Transcriptome Profiling",
                   data.type = "Gene Expression Quantification", 
@@ -63,24 +63,26 @@ end.time <- Sys.time ()
 time <- end.time - start.time #4.850641 mins
 print (time)
 
-# prepare and save data into a SummarizedExperiment
+## prepare and save data into a SummarizedExperiment
 dir.create(Download.path, showWarnings = FALSE)
 data <- GDCprepare(query,
                    save = TRUE,
                    save.filename = paste0(Download.path,Download.file,".RDA"))
-#load("./2 DATA/TCGA RNAseq/RNASeq_OV_BIOLINKS/OV.RNASeq.TCGA.BIOLINKS.DATA.RDA")
+#load(paste0("./2 DATA/TCGA RNAseq/RNASeq_OV_BIOLINKS/",Cancerset,".RNASeq.TCGA.BIOLINKS.DATA.RDA"))
 
-# Extract the Expression matrix
+## Extract the Expression matrix
 Matrix <- assay(data,"raw_counts")
 
-#load("./2 DATA/TCGA RNAseq/RNASeq_OV_BIOLINKS/OV.RNASeq.TCGA.BIOLINKS.DATA.RDA")
-
-#Clinical Data
+## EDAseq normalisation
+dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
+                                      geneInfo = geneInfo,
+                                      method = "gcContent")
+# Clinical Data
 dir.create(paste0("./2 DATA/Clinical Information/",Cancerset,"/BIOLINKS/"), showWarnings = FALSE)
-#indexed GDC
+## indexed GDC
 clin <- GDCquery_clinic(paste0(download.source,"-",Cancerset), type = "clinical", save.csv = FALSE)
 write.csv (clin,file=paste0("./2 DATA/Clinical Information/",Cancerset,"/BIOLINKS/indexed.GDC.clinicaldata.csv"))
-#XML parsed
+## XML parsed
 clin.query <- GDCquery(project = paste0(download.source,"-",Cancerset), 
                        data.category = "Clinical")
 GDCdownload(clin.query)
@@ -89,6 +91,9 @@ clinical.follup_up <- GDCprepare_clinic(clin.query, clinical.info = "follow_up")
 write.csv (clinical.patient,file=paste0("./2 DATA/Clinical Information/",Cancerset,"/BIOLINKS/Patient.XML.clinicaldata.csv"))
 write.csv (clinical.follup_up,file=paste0("./2 DATA/Clinical Information/",Cancerset,"/BIOLINKS/Follow_up.XML.clinicaldata.csv"))
 
+## subtype data
 Subtype.available <- c("ACC","BRCA", "COAD","GBM","HNSC","KICH","KIRP","KIRC","LGG","LUAD", "LUSC", "PRAD", "PANCAN","READ","SKCM","STAD","THCA","UCEC")
 if (Cancerset %in% Subtype.available) {subtypes <- TCGAquery_subtype(tumor = Cancerset)}
+
+
 
