95e369e078c2deffaf82116cca196fdfe29ca8e3
Wouter <wouter@cse-node-01.sfdrwbdyldxura5qb1jwayl5dc.bx.internal.cloudapp.net>
2016-09-07 08:57:12 +0000

    exta one

--- Any Cancer/2.1.Normalize.EDASeq.ANY-CANCER.v3.R
+++ Any Cancer/2.1.Normalize.EDASeq.ANY-CANCER.v3.R
@@ -26,14 +26,18 @@ library("mygene")
 source("/mnt3/wouter/QCRI-SIDRA-ICR/R tools/stefanofunctions.R")
    
 # Set Parameters
-Cancerset = "PRAD"
-DL.Method = "BIOLINKS" #Choose "ASSEMBLER" or "BIOLINKS"
+Cancerset    = "UVM"
+DL.Method    = "BIOLINKS" #Choose "ASSEMBLER" or "BIOLINKS"
+sample.types = "Selected" #Alternatives TP , TP_TM , Selected
+
 Parent.Cancerset <- substring(Cancerset,1,4)
 Seq.tech = ""
 if (substring(Cancerset,6,nchar(Cancerset))=="hiseq") {Seq.tech = ".hiseq"}
 if (substring(Cancerset,6,nchar(Cancerset))=="GA") {Seq.tech = ".GA"}
 
 dir.create(paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/"), showWarnings = FALSE)
+
+print (paste0("Normalizing ",DL.Method," ",Cancerset ," RNASeq Data, using ",sample.types," sample types."))
 
 # Load RAW Level 3 RNASeq Data downloaded with TCGA Assembler or TCGA Biolinks
 if (DL.Method=="ASSEMBLER"){
@@ -84,32 +88,25 @@ if (DL.Method=="ASSEMBLER"){
   #load(paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.ASSEMBLER.PRENORM.RData"))                            # Restore prenormalisation Data
 }
 if (DL.Method=="BIOLINKS"){
-  load(paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_BIOLINKS/",Cancerset,".RNASeq.TCGA.",DL.Method,".DATA.RDA"))
+  load(paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_BIOLINKS/",Cancerset,".RNASeq.TCGA.",DL.Method,".",sample.types,".DATA.RDA"))
   RNASeq.DATA.matrix <- assay(data)
-  gene.IDs <- queryMany(rownames(RNASeq.DATA.matrix), scopes="ensembl.gene", fields=c("symbol","entrezgene"), species="human")
-  gene.table <- as.data.frame(gene.IDs)
-  #remove double samples
-  Douplicate.patients <- unique(substr(colnames(RNASeq.DATA.matrix)[which(duplicated(substr(colnames(RNASeq.DATA.matrix),1,12)))],1,12))
-  if (length(Douplicate.patients)>0){
-    print ("Dataset contains mutiple samples for these patients :")
-    print (Douplicate.patients)
-    RNASeq.DATA.matrix <-  RNASeq.DATA.matrix[,-which((substr(colnames(RNASeq.DATA.matrix),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.DATA.matrix),14,16)=="01B"))] # drop duplicated FPE
-    RNASeq.DATA.matrix <-  RNASeq.DATA.matrix[,-which((substr(colnames(RNASeq.DATA.matrix),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.DATA.matrix),22,25)=="A277"))] # drop A277 reps
-  }
-  Patient.IDs <- substr(colnames(RNASeq.DATA.matrix),1,12)
+  #rename genes from ENSEMBLE TO HUGO SYMBOL
+  #gene.IDs <- queryMany(rownames(RNASeq.DATA.matrix), scopes="ensembl.gene", fields=c("symbol","entrezgene"), species="human")
+  #gene.table <- as.data.frame(gene.IDs)
+  #save (gene.table,file="./2 DATA/gene.table.RNASeq.RData")
+  load (file="./2 DATA/gene.table.RNASeq.RData")
   RNASeq.DATA.table <- as.data.frame(RNASeq.DATA.matrix)
-  colnames(RNASeq.DATA.table) <- Patient.IDs
-  #rename genes from ENSEMBLE TO HUGO SYMBOL 
   RNASeq.DATA.table$gene.name <- gene.table$symbol[match(rownames(RNASeq.DATA.matrix),gene.table$query)]
   RNASeq.DATA.table <- RNASeq.DATA.table[-which(is.na(RNASeq.DATA.table$gene.name)),]
   RNASeq.DATA.table$gene.name[which(duplicated(RNASeq.DATA.table$gene.name))] <- paste0(RNASeq.DATA.table$gene.name[which(duplicated(RNASeq.DATA.table$gene.name))],
                                                                                         "_",rownames(RNASeq.DATA.table[which(duplicated(RNASeq.DATA.table$gene.name)),]))
   rownames(RNASeq.DATA.table) <- RNASeq.DATA.table$gene.name
   RNASeq.DATA.table$gene.name <- NULL
+  
   #generate numeric matrix
   RNASeq.DATA <- as.matrix(RNASeq.DATA.table)
   mode(RNASeq.DATA) <- "numeric"
-  save(RNASeq.DATA,file=paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".PRENORM.RData"))             # prenormalized version
+  save(RNASeq.DATA,file=paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".",sample.types,".PRENORM.RData"))             # prenormalized version
   #load(paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".PRENORM.RData")) 
 }
   
@@ -130,9 +127,41 @@ RNASeq.NORM <-  floor(exp(RNASeq.NORM) - .1)
 #RNASeq.NORM.quantiles <- normalize.quantiles(RNASeq.NORM)                       ## Alternative function using "preprocessCore" vs "stefanofunctions.R" , Process will take very long time
 RNASeq.NORM.quantiles <- t(quantileNormalization(t(RNASeq.NORM)))
 RNASeq.NORM <- floor(RNASeq.NORM.quantiles)
-RNASeq.NORM_Log2<-log(RNASeq.NORM+1,2)
 
 # Save Data
-save(RNASeq.NORM,file=paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".NORMALIZED.RData"))             # without log2 transformation
-save(RNASeq.NORM_Log2,file=paste0("././2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".NORMALIZED.LOG2.RData")) # with log2 transformation: the matrix to use
+save(RNASeq.NORM,file=paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".",sample.types,".NORMALIZED.RData"))             # without log2 transformation
 
+# filter out TP samples
+RNASeq.NORM.TP <- RNASeq.NORM[,which(substr(colnames(RNASeq.NORM),14,15)=="01")]
+#remove double samples
+Douplicate.patients <- unique(substr(colnames(RNASeq.NORM.TP)[which(duplicated(substr(colnames(RNASeq.NORM.TP),1,12)))],1,12))
+if (length(Douplicate.patients)>0){
+  print ("Dataset contains mutiple samples for these patients :")
+  print (Douplicate.patients)
+  if (length(which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),14,16)=="01B")))>0){
+    RNASeq.NORM.TP <-  RNASeq.NORM.TP[,-which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),14,16)=="01B"))] # drop duplicated FPE
+    Douplicate.patients <- unique(substr(colnames(RNASeq.NORM.TP)[which(duplicated(substr(colnames(RNASeq.NORM.TP),1,12)))],1,12))
+  }
+  if (length(which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),22,25)=="A277")))>0){
+    RNASeq.NORM.TP <-  RNASeq.NORM.TP[,-which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),22,25)=="A277"))] # drop A277 reps
+    Douplicate.patients <- unique(substr(colnames(RNASeq.NORM.TP)[which(duplicated(substr(colnames(RNASeq.NORM.TP),1,12)))],1,12))
+  }
+  if (length(which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),18,20)=="02R")))>0){
+    RNASeq.NORM.TP <-  RNASeq.NORM.TP[,-which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),18,20)=="02R"))] # drop 02R reps
+    Douplicate.patients <- unique(substr(colnames(RNASeq.NORM.TP)[which(duplicated(substr(colnames(RNASeq.NORM.TP),1,12)))],1,12))
+  }
+  if (length(which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),22,25)=="A278")))>0){
+    RNASeq.NORM.TP <-  RNASeq.NORM.TP[,-which((substr(colnames(RNASeq.NORM.TP),1,12) %in% Douplicate.patients) & (substr(colnames(RNASeq.NORM.TP),22,25)=="A278"))] # drop A277 reps
+    Douplicate.patients <- unique(substr(colnames(RNASeq.NORM.TP)[which(duplicated(substr(colnames(RNASeq.NORM.TP),1,12)))],1,12))
+  }
+  if (length(Douplicate.patients)>0){
+    print ("Unable to remove mutiple samples for these patients :")
+    print (Douplicate.patients)
+  }
+}
+Patient.IDs <- substr(colnames(RNASeq.NORM.TP),1,12)
+colnames(RNASeq.NORM.TP) <- Patient.IDs
+
+RNASeq.NORM.TP_Log2<-log(RNASeq.NORM.TP+1,2)
+
+save(RNASeq.NORM.TP_Log2,file=paste0("./2 DATA/TCGA RNAseq/RNASeq_",Cancerset,"_EDASeq/",Cancerset,".RNASeq.TCGA.",DL.Method,".",sample.types,".NORMALIZED.TP_FILTERED_LOG2.RData")) # with log2 transformation: the matrix to use
